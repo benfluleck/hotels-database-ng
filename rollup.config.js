cc479471ea1113a60ac2e16c0ad35ceaf81986cf
Benny Ogidan <bennyogidan@Andelas-MacBook-Pro-8.local>
2019-01-10 23:00:19 +0100

    chore(implement-rollup): Implement rollup config

---
+++ rollup.config.js
@@ -0,0 +1,15 @@
+const dist = 'dist'
+
+export default {
+  input: 'src/index.js',
+  output: [
+    {
+      file: `${dist}/bundle.esm.js`,
+      format: 'esm'
+    },
+    {
+      file: `${dist}/bundle.cjs.js`,
+      format: 'cjs'
+    }
+  ]
+}
