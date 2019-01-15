159d3d5221ab7e6752b6fbf58a6fb3248328b5e6
rotimi-babalola <rotimi.babalola@andela.com>
2019-01-15 17:04:40 +0100

    fix sequelize warning

--- src/config/config.js
+++ src/config/config.js
@@ -1,9 +1,21 @@
+const Sequelize = require('sequelize')
+const Op = Sequelize.Op
+
 require('dotenv').config()
 
 module.exports = {
   development: {
     url: process.env.DATABASE_URL,
-    dialect: 'postgres'
+    dialect: 'postgres',
+    operatorsAliases: {
+      $and: Op.and,
+      $or: Op.or,
+      $eq: Op.eq,
+      $gt: Op.gt,
+      $lt: Op.lt,
+      $lte: Op.lte,
+      $like: Op.like
+    }
   },
   test: {
     username: 'root',
