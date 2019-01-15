dd534f7753f154874f1d87bca51124522d4a669e
rotimi-babalola <rotimi.babalola@andela.com>
2019-01-15 21:50:38 +0100

    PR feedback

--- src/config/config.js
+++ src/config/config.js
@@ -1,20 +1,13 @@
-const Sequelize = require('sequelize')
-const Op = Sequelize.Op
+const operatorsAliases = require('./operatorsAliases')
 
-require('dotenv').config()
+console.log(process.env, 'env')
 
 module.exports = {
   development: {
     url: process.env.DATABASE_URL,
     dialect: 'postgres',
     operatorsAliases: {
-      $and: Op.and,
-      $or: Op.or,
-      $eq: Op.eq,
-      $gt: Op.gt,
-      $lt: Op.lt,
-      $lte: Op.lte,
-      $like: Op.like
+      ...operatorsAliases
     }
   },
   test: {
@@ -22,13 +15,6 @@ module.exports = {
     password: null,
     database: 'database_test',
     host: '127.0.0.1',
-    dialect: 'postgres'
-  },
-  production: {
-    username: 'root',
-    password: null,
-    database: 'database_production',
-    host: '127.0.0.1',
-    dialect: 'postgres'
+    dialect: 'sqlite'
   }
 }
--- src/models/dummyhotel.js
+++ src/models/dummyhotel.js
@@ -1,14 +1,18 @@
-'use strict';
+'use strict'
 module.exports = function(sequelize, DataTypes) {
-  var DummyHotel = sequelize.define('DummyHotel', {
-    name: DataTypes.STRING,
-    location: DataTypes.STRING
-  }, {
-    classMethods: {
-      associate: function(models) {
-        // associations can be defined here
+  var DummyHotel = sequelize.define(
+    'DummyHotel',
+    {
+      name: DataTypes.STRING,
+      location: DataTypes.STRING
+    },
+    {
+      classMethods: {
+        associate: function(models) {
+          // associations can be defined here
+        }
       }
     }
-  });
-  return DummyHotel;
-};
+  )
+  return DummyHotel
+}
